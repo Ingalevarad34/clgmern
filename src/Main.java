a26bc27b3fb3b9c226a43bed4802d02cad25aa92
Ingale Varad <117982922+Ingalevarad34@users.noreply.github.com>
2026-03-31 16:54:34 +0530

    Sync room 8UBL7P from NeonCode Live

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in).useDelimiter("\\A");
+        String input = sc.hasNext() ? sc.next() : "";
+        System.out.println(input.trim());
+    }
+}
